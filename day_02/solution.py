644f6610e58813aa82d9624a1b7ef9623b35d0fa
UltraGnome <ultragnomeultra@duck.com>
2025-12-02 15:28:53 -0700

    Day 2 Part 2 solution

--- day_02/solution.py
+++ day_02/solution.py
@@ -8,20 +8,14 @@ class Part1:
         result = 0
         range_list = file_lines[0].split(',')
 
-        print(len(range_list))
-
         for minmax in range_list:
             min_max = minmax.split('-')
             min: int = int(min_max[0])
             max: int = int(min_max[1])
 
-            for i in range(min,max):
-                # The regex pattern (.+)\1+ looks for:
-                # (.+) : Any character (except newline) one or more times, captured in group 1.
-                # \1+  : One or more occurrences of the content captured in group 1.
-                # We add a check for digits specifically by ensuring the captured group only contains digits.
-                # This is done by adding \d+ inside the capturing group.
-                if bool(re.search(r'(\d+)\1+', str(i))):
+            for i in range(min,max + 1):
+
+                if bool(re.search(r'^(.*)\1$', str(i))):
                     print(f'funky data found : {i}' )
                     result += i
 
@@ -31,21 +25,31 @@ class Part1:
 class Part2:
     @staticmethod
     def solution(file_lines: list[str]) -> int:
+        range_list = file_lines[0].split(',')
         result = 0
 
-        for line in file_lines:
+        for minmax in range_list:
+            min_max = minmax.split('-')
+            min: int = int(min_max[0])
+            max: int = int(min_max[1])
+
+            for i in range(min,max + 1):
+
+                if bool(re.search(r'^(\d+)\1+$', str(i))):
+                    print(f'funky data found : {i}' )
+                    result += i
 
 
-          return result
+        return result
 
 
 
-with open("test_input.txt", "r") as file:
-# with open("input.txt", "r") as file:
+# with open("test_input.txt", "r") as file:
+with open("input.txt", "r") as file:
     f = file.read().splitlines()
 
-print(f"Part 1: {Part1.solution(f)}")
-# print(f"Part 2: {Part2.solution(f)}")
+# print(f"Part 1: {Part1.solution(f)}")
+print(f"Part 2: {Part2.solution(f)}")
 
 
 
