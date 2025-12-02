43c7546a0b6749fd76db57c1d9df6b559a836700
UltraGnome <ultragnomeultra@duck.com>
2025-12-01 21:39:03 -0700

    Day 1 Part 2 solution

--- day_01/solution.py
+++ day_01/solution.py
@@ -36,49 +36,34 @@ class Part1:
 class Part2:
 
     def solution(file_lines: list[str]) -> int:
-        result = 0
         dial_value = 50
-
+        result = 0
         for line in file_lines:
-            previous_dial_value = dial_value
-            if not line or len(line) < 2:
-                print(f'error: invalid line "{line}"')
-                continue
-
+            line = line.strip()
             direction = line[0]
-            try:
-                step = int(line[1:])
-                spins = int(step / 100)
-                result += spins
-            except ValueError:
-                print(f'error: invalid number in "{line}"')
-                continue
-
+            step = int(line[1:])
             if direction == 'L':
-                 dial_value = (dial_value - step) % 100
-                 if previous_dial_value > dial_value & dial_value != 0:
-                     result += 1
-
-            elif direction == 'R':
-                dial_value = (dial_value + step) % 100
-                if previous_dial_value < dial_value & dial_value != 0:
+                new_position = (dial_value - step) % 100
+                if new_position > dial_value != 0:
                     result += 1
-
+            elif direction == 'R':
+                new_position = (dial_value + step) % 100
+                if new_position < dial_value and new_position != 0:
+                    result += 1
             else:
-                print(f'error: unknown direction in "{line}" dial={dial_value}')
-                continue
+                raise ValueError("Invalid direction.")
+
+            dial_value = new_position
 
             if dial_value == 0:
                 result += 1
-                print(f"result: {result}")
-
-            print(f"dial_value: {dial_value}")
+            number_of_rotations = step // 100
+            result += number_of_rotations
 
         return result
 
-
-with open("test_input2.txt", "r") as file:
-# with open("input.txt", "r") as file:
+# with open("test_input2.txt", "r") as file:
+with open("input.txt", "r") as file:
     f = file.read().splitlines()
 
 # print(f"Test input: {Part1.solution(f)}")
